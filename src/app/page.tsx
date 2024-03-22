2ac6d2f8dad77d17469a21720ead0e88a488084c
JayvonK <jaykhuth@gmail.com>
2024-03-22 15:51:07 -0700

    day 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import '@/app/css/LoginPage.css'
 
 export default function Home() {
   const [openerBool, setOpenerBool] = useState<boolean>(true);
@@ -20,6 +21,12 @@ export default function Home() {
           openerBool ? ( // First Ternary Return Statement
 
             <div className="min-h-screen bg-black opacity-90" onClick={handleOpenerBoolChange}>
+              <div className="px-80 pt-60">
+                <h1 className="txtOrange text-center text-8xl juraBold mb-16"> Strike <span className="text-white">Showdown</span></h1>
+                <h2 className="text-center text-white jura txtOrange text-5xl mb-14">the best social app for bowlers</h2>
+                <h3 className="text-center text-white jura text-5xl mb-24">Where you’ll be able to create or join 1v1 challenges or practice sessions with other bowlers!</h3>
+                <h3 className="text-center text-white jura txtOrange text-5xl">click to continue...</h3>
+              </div>
 
             </div>
 
