5eba01e80b09117b884181b51350ac450aa7a55e
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-04-08 14:55:56 -0700

    Working on modal

--- src/components/PageComponents/ModelComponent.tsx
+++ src/components/PageComponents/ModelComponent.tsx
@@ -7,29 +7,28 @@ export const ModelComponent = () => {
     const [openModal, setOpenModal] = useState(false);
   return (
     <>
-      <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
-      <Modal show={openModal} onClose={() => setOpenModal(false)}>
-        <Modal.Header>Terms of Service</Modal.Header>
-        <Modal.Body>
-          <div className="space-y-6">
+
+      <Button className='rounded-3xl ' onClick={() => setOpenModal(true)}>Toggle modal</Button>
+      <Modal   className=' bg-black border-0 '   show={openModal} onClose={() => setOpenModal(false)}>
+        <Modal.Header style={{borderTopLeftRadius:'100px'}}  className='  border-0 rounded-t-3xl '>Title</Modal.Header>
+        <Modal.Body  className='border-0 '>
+          <div className="space-y-6 ">
             <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              With less than a month to go before the European Union enacts new consumer privacy laws for its citizens,
-              companies around the world are updating their terms of service agreements to comply.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima veritatis vel dolore tempora modi consectetur enim natus distinctio repellendus officiis voluptas voluptate neque perferendis, placeat sunt repudiandae consequatur? Unde, quae.
             </p>
             <p className="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-              The European Union’s General Data Protection Regulation (G.D.P.R.) goes into effect on May 25 and is meant
-              to ensure a common set of data rights in the European Union. It requires organizations to notify users as
-              soon as possible of high-risk data breaches that could personally affect them.
+           Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero impedit corporis aut aliquid sit consequuntur quod nihil necessitatibus voluptate modi asperiores, repellat delectus! Possimus est qui, cumque cupiditate architecto mollitia.
             </p>
           </div>
         </Modal.Body>
-        <Modal.Footer>
-          <Button onClick={() => setOpenModal(false)}>I accept</Button>
-          <Button color="gray" onClick={() => setOpenModal(false)}>
-            Decline
+        <Modal.Footer className='flex justify-end'  >
+          {/* <Button onClick={() => setOpenModal(false)} >Close</Button> */}
+          <Button style={{backgroundColor:'FF7A00',}} className=' rounded-xl text-black jura !imporant-hover:bg-orange-500 bg-orange-500 ' onClick={() => setOpenModal(false)}>
+          <h4 className='text-2xl'>Close</h4> 
           </Button>
         </Modal.Footer>
       </Modal>
+
     </>
   );
 }
--- src/components/PageComponents/NavBarComponent.tsx
+++ src/components/PageComponents/NavBarComponent.tsx
@@ -1,7 +1,6 @@
-
 "use client";
 import Link from "next/link";
-import { Navbar } from "flowbite-react";
+import { Navbar, Dropdown, Avatar } from "flowbite-react";
 import { useState } from "react";
 import logo from "../../../public/images/Strike Showdown Logo.png";
 import SearchIcon from "../../../public/images/Search.png";
@@ -9,167 +8,185 @@ import FriendsIcon from "../../../public/images/Property 1=Default (1).png";
 import InboxIcon from "../../../public/images/Inbox.png";
 import MessageIcon from "../../../public/images/Message.png";
 import ProfileIcon from "../../../public/images/Profile.png";
+import { ModelComponent } from "./ModelComponent";
 import Image from "next/image";
 
 import "../../app/css/LoginPage.css";
 import { useRouter } from "next/navigation";
 
-const NavBar = () => {
+
+
+
+function NavBarComponent() {
+
+  const NavBar = () => {
     const [isModalOpen, setIsModalOpen] = useState(false);
   
     const toggleModal = () => {
       setIsModalOpen(!isModalOpen);
     };
-}
- 
-
-function NavBarComponent() {
-
-    const router = useRouter();
-  return (
-    <nav className="bg-black  dark:bg-gray-900 ">
-    <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
-      <a 
-     href=""
-        className="flex items-center space-x-3 rtl:space-x-reverse "
-      >
-        <img
-          src={logo.src}
-          alt=""
-          className="object-cover h-12 w-12 md:h-20 md:w-20 lg:h-20 lg:w-20  rounded-full border border-white hover:cursor-pointer"onClick={() => router.push('/')}
-        />
-        <div className="pl-4">
-          <h1 className="txtOrange text-center lg:text-4xl md:text-3xl text-2xl juraBold mt-0 md:mt-2 pl-1 md:pl-5 ">
-            {" "}
-            Strike <span className="text-white">Showdown</span>
-          </h1>
-        </div>
-      </a>
-      <button
-        data-collapse-toggle="navbar-default"
-        type="button"
-        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg lg:hidden  focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-default"
-        aria-expanded="false"
-      >
-        <span className="sr-only">Open main menu</span>
-        <svg
-          className="w-5 h-5"
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 17 14"
-        >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M1 1h15M1 7h15M1 13h15"
-          />
-        </svg>
-      </button>
-      <div
-        className="hidden w-full lg:block md:w-auto row-auto ml-10 flex items-baseline "
-        id="navbar-default"
-      >
-        <ul className="font-medium flex flex-col items-end p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700" >
-          <li className="hover:brightness-100 hover:cursor-pointer mr-4 ">
-            <img
-              src={SearchIcon.src}
-              alt=""
-              className="object-cover h-15 w-15 hover:cursor-pointer showcase-menu-social mb-1"
-            />
-            <a className="text-1xl text-white text-center  hover:text-orange-500  jura">
-              Search
-            </a>
-          </li>
-          <li className="  hover:cursor-pointer hover:brightness-95    "onClick={() => router.push('/')}>
-            <img
-              src={FriendsIcon.src}
-              alt=""
-            
-              className="object-cover h-15 mb-1  showcase-menu-social"
-            />
-            <a className="text-1xl text-white text-center  hover:text-orange-500 jura">
-              Friends
-            </a>
-          </li>
-          <li className="hover:brightness-105 hover:cursor-pointer mr-4 "onClick={() => router.push('')}>
-            <img
-              src={InboxIcon.src}
-              alt=""
-              className="object-cover h-15 mb-1  "
-            />
-            <a className="text-1xl text-white text-center  hover:text-orange-500  jura">
-              Inbox
-            </a>
-          </li>
-          <li className="hover:brightness-100 hover:cursor-pointer "onClick={() => router.push('')}>
-            <img
-              src={MessageIcon.src}
-              alt=""
-              className="object-cover pl-2 h-15 mb-1 "
-            />
-
-            <a className="text-1xl text-white text-center hover:cursor-pointer hover:text-orange-500 jura">
-              Message
-            </a>
-          </li>
-          <li className="hover:brightness-100 hover:cursor-pointer "onClick={() => router.push('/')}>
-            <img
-              src={ProfileIcon.src}
-              alt=""
-              className="object-cover h-15 hover:cursor-pointer mb-1 "
-            />
-            <a className="text-1xl text-white text-center hover:gray-100  hover:text-orange-500  jura">
-              Profile
-            </a>.
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-//     <Navbar fluid
-//   className="bg-black  dark:bg-gray-900  ">
-//         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
-//       <Navbar.Brand as={Link}  href="https://flowbite-react.com">
-//         <img src={logo.src} className="object-cover h-12 w-12 md:h-20 md:w-20 lg:h-20 lg:w-20  rounded-full border border-white hover:cursor-pointer" alt="Flowbite React Logo" />
-//         <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">Flowbite React</span>
-//       </Navbar.Brand>
-//       <Navbar.Toggle />
-//       <Navbar.Collapse>
-//         <Navbar.Link className="text-1xl text-white text-center  hover:text-orange-500 navbarHover  jura" href="#" >
-//           Home
-//         </Navbar.Link>
-//         <Navbar.Link className="text-1xl text-white text-center  hover:text-orange-500 navbarHover  jura" as={Link} href="#">
-//           About
-//         </Navbar.Link>
-//         <Navbar.Link className="text-1xl text-white text-center  hover:text-orange-500 navbarHover  jura" >Services</Navbar.Link>
-//         <Navbar.Link className="text-1xl text-white text-center  hover:text-orange-500 navbarHover jura"  >Pricing</Navbar.Link>
-
-//         <Navbar.Link className="text-1xl text-white text-center  hover:text-orange-500 navbarHover  jura" href="#"> <img
-//                   src={MessageIcon.src}
-//                   alt=""
-//                   className="object-cover pl-2 h-15 mb-1 "
-//                 />
-// Contact </Navbar.Link>
-//       </Navbar.Collapse>
-//       </div>
-//     </Navbar>
+  };
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
     
+    const OpenNav = () => {
+      setIsNavOpen(!isNavOpen);
+    };
+  
+  const router = useRouter();
+  return (
+ 
+    <nav className="bg-black  dark:bg-gray-900 ">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4 ">
+        <a href="" className="flex items-center space-x-3 rtl:space-x-reverse ">
+          <img
+            src={logo.src}
+            alt=""
+            className="object-cover h-12 w-12 md:h-20 md:w-20 lg:h-20 lg:w-20  rounded-full border border-white hover:cursor-pointer"
+            onClick={() => router.push("/")}
+          />
+          <div className="pl-4">
+            <h1 className="txtOrange text-center lg:text-4xl md:text-3xl text-2xl juraBold mt-0 md:mt-2 pl-1 md:pl-5 ">
+              {" "}
+              Strike <span className="text-white">Showdown</span>
+            </h1>
+          </div>
+        </a>
+  
+        <button onClick={()=> setIsNavOpen(true)}
+          data-collapse-toggle="navbar-default"
+          type="button"
+          className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+          aria-controls="navbar-default"
+          aria-expanded="false"
+        >
+          <span className="sr-only">Open main menu</span>
+          <svg
+            className="w-5 h-5"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+    
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+         
+          </svg>
+
+         
+        </button>
+  
+    
+        
+        <div
+          className="hidden w-full lg:block md:w-auto row-auto ml-10 flex items-baseline "
+          id="navbar-default"
+        >
+          <ul className="font-medium flex flex-col gap-6 items-end p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0  dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li className=" hover:cursor-pointer  hover:brightness-125">
+              <a className="text-1xl text-white text-center  hover:text-orange-500  jura">
+                <img
+                  src={SearchIcon.src}
+                  alt=""
+                  className="object-cover h-15 w-15 hover:cursor-pointer showcase-menu-social mb-1 "
+                />
+Search
+              </a>
+            </li>
+            <li
+              className="  hover:cursor-pointer hover:brightness-125    "
+              onClick={() => router.push("/")}
+            >
+              <a className="text-1xl text-white text-center  hover:text-orange-500 jura">
+                <img
+                  src={FriendsIcon.src}
+                  alt=""
+                  className="object-cover pr-1 justify-center  h-14 w-16 mb-1  showcase-menu-social"
+                />
+               Friends
+              </a>
+            </li>
+            <li
+              className="hover:brightness-125 hover:cursor-pointer mr-4 "
+              onClick={() => router.push("")}
+            >
+              <a className="text-1xl text-white text-center  hover:text-orange-500  jura">
+                <img
+                  src={InboxIcon.src}
+                  alt=""
+                  className="object-cover h-15 mb-1  "
+                />
+                Inbox
+              </a>
+            </li>
+            <li
+              className="hover:brightness-125 hover:cursor-pointer "
+              onClick={() => router.push("")}
+            >
+              <a className="text-1xl text-white text-center hover:cursor-pointer  hover:text-orange-500 jura">
+                <img
+                  src={MessageIcon.src}
+                  alt=""
+                  className="object-cover pl-2 h-15 mb-1 "
+                />
+               Message
+              </a>
+            </li>
+            <li
+              className="hover:brightness-125 hover:cursor-pointer "
+              onClick={() => router.push("/")}
+            >
+              <a className="text-1xl text-white text-center hover:gray-100  hover:text-orange-500  jura">
+                <img
+                  src={ProfileIcon.src}
+                  alt=""
+                  className="object-cover  h-15 hover:cursor-pointer mb-1 "
+                />
+               Profile
+              
+              </a>
+              
+              .
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   );
 }
-//  function t() {
-//     return(
-   
-//     )
- 
-// }
-     
-     
 
-
+  {/*
+         <Dropdown label="Dropdown button" onClick={()=>(setIsNavOpen(false)) } dismissOnClick={false}>
+      <Dropdown.Item>Dashboard</Dropdown.Item>
+      <Dropdown.Item>Settings</Dropdown.Item>
+      <Dropdown.Item>Earnings</Dropdown.Item>
+      <Dropdown.Item>Sign out</Dropdown.Item>
+    </Dropdown>
+        
+        
+        
+        <Dropdown onClick={()=>(setIsNavOpen(false)) }
+          arrowIcon={false}
+          inline
+          label={
+            <Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded />
+          }
+        >
+          <Dropdown.Header>
+            <span className="block text-sm">Bonnie Green</span>
+            <span className="block truncate text-sm font-medium">name@flowbite.com</span>
+          </Dropdown.Header>
+          <Dropdown.Item>Dashboard</Dropdown.Item>
+          <Dropdown.Item>Settings</Dropdown.Item>
+          <Dropdown.Item>Earnings</Dropdown.Item>
+          <Dropdown.Divider />
+          <Dropdown.Item>Sign out</Dropdown.Item>
+        </Dropdown> */}
+       
 export default NavBarComponent;
-
