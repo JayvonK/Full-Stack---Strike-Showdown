2dbfd2a50dfc0d0080395579c8019f14ff1e7dbe
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-04-09 15:23:28 -0700

    modals

--- src/components/PageComponents/NavBarComponent.tsx
+++ src/components/PageComponents/NavBarComponent.tsx
@@ -11,8 +11,7 @@ import ProfileIcon from "../../../public/images/Profile.png";
 import { Button, Modal,Avatar } from "flowbite-react";
 import UserMinus  from "../../../public/images/UserMinus.png";
 import ProfilePic from"../../../public/images/Ellipse 16.png";
-import
-import { ModelComponent } from "./ModelComponent";
+import ProfilePic2 from"../../../public/images/profilePIc.png";
 import Image from "next/image";
 
 import { Label, TextInput } from "flowbite-react";
@@ -165,46 +164,70 @@ function NavBarComponent() {
 </Modal>
  
 <Modal show={openModal2} onClose={() => setOpenModal2(false)}>
-  <Modal.Header className=" " >
-     <h3 className="text-2xl bg-orange-500 rounded-2xl hover:!bg-orange-500 text-black jura px-4 pt-3 pb-3">ff</h3> 
-   </Modal.Header>
-  <Modal.Body>
-    <div className="grid grid-flow-col p-4  justify-evenly bg-black rounded-2xl w-50"style={{width:300}}>
-    
-    <div className="">
-   <img  src={ProfilePic.src}></img>
-        
+
+  <Modal.Body className=" flex justify-end">
+  <div>
+    <img  src={ProfilePic2.src} className="pr-2"></img>
     </div>
-  <div className="ml-5">
+    <div className="grid grid-flow-col pt-4 pb-4 pr-5   bg-black rounded-2xl w-50"style={{width:400}}>
+
+    <div>
+   
+</div>
+<div>
+  
+</div>
+   
+  <div className="ml-5 ">
  
     <div className="grid grid-flow-col auto-cols-max justify-start items-center pb-3 ">
     
+   
     <div>
-    <img onClick={()=>{}} src={UserMinus.src} className="pr-2"></img>
-    </div>
-    <div>
-    <h1 className="text-white jura text-xl ">BowlerDude</h1>
+    <h1 className="text-white jura text-3xl pb-4 juraBold">KingOfBowling209</h1>
     
+    <h1 className="text-white jura text-2xl ">He/Him</h1>
     </div>
   
     </div>
-  <button style={{width:150}}  className=" bg-orange-500  hover:!bg-orange-500 mb-2 text-black jura" >
-     <h3 className="text-1xl">Challenge</h3> 
+  
+      <div className="justify-center">
+  <button style={{width:150}}  className="  bg-orange-500 mt-5 rounded-2xl pt-2 pb-2  hover:!bg-orange-500 mb-2 text-black jura" >
+     <h3 className="text-3xl">Edit</h3> 
     </button>
-    <br>
-    </br>
-    <button style={{width:150}} className=" bg-orange-500   hover:!bg-orange-500 text-black jura">
-     <h3 className="text-1xl">Message</h3> 
+  
+    <button style={{width:150}} className="bg-red-500 ml-10 rounded-2xl pt-2 pb-2  text-black jura">
+     <h3 className="text-3xl">Log Out</h3> 
     </button>
+    </div>
+    </div>
   </div>
 
-    </div>
-  </Modal.Body >
-  <Modal.Footer className=" flex justify-end ">
+
     
-    <Button className=" bg-orange-500 rounded-2xl hover:!bg-orange-500 text-black jura" onClick={() => setOpenModal(false)}>
-     <h3 className="text-2xl">Close</h3> 
-    </Button>
+  </Modal.Body >
+  <Modal.Footer>
+<div  className="rounded-xl flex justify-space bg-black w-50 px-5 grid grid-flow-col auto-cols" style={{width:600}}>
+<div className="">
+<h1 className=" bg-orange-500 text-3xl text-center mt-5 rounded-xl   pt-2 pb-2  mb-2 text-black jura"style={{width:150}}>Info</h1>
+<p className="text-white jura text-1xl">Full Name:</p>
+<p className="text-orange-500 jura text-2xl">Timothy Neutron</p>
+<p className="text-white jura text-1xl">Main Center:</p>
+<p className="text-orange-500 jura text-2xl">Pacific Avenue Bowl</p>
+
+</div>
+<div className="">
+<h1 className="text-white jura text-3xl  mt-5 rounded-xl   pt-2 pb-2  mb-2 text-black jura"style={{width:150}}>Your Post</h1>
+<p className="text-white jura text-1.5xl">Style:</p>
+<p className="text-orange-500 jura text-2xl">2 Handed (Righty)</p>
+<p className="text-white jura text-1.5xl">Average:</p>
+<p className="text-orange-500 jura text-2xl">192.5</p>
+</div>
+
+
+
+</div>
+    
   </Modal.Footer>
 </Modal>
 
