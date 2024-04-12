9830d3742d374772533f79bb2c0aea86b6ba5f7f
JayvonK <jaykhuth@gmail.com>
2024-04-12 13:42:29 -0700

    save

--- src/components/PageComponents/NavBarComponent.tsx
+++ src/components/PageComponents/NavBarComponent.tsx
@@ -49,14 +49,16 @@ router.push('/');
       fluid
       className="bg-black  !border-black pr-0 xl:!pr-32 xl:!pl-52 items-center     " theme={customTheme}
     >
-      <Navbar.Brand  as={Link} href="/">
+      <Navbar.Brand as={Link} href="/pages/HomePage" >
         <img
           src={logo.src}
           className="mr-3 object-cover   h-12 w-12 md:h-14 md:w-14 xl:h-20 xl:w-20  rounded-full border border-white hover:cursor-pointer"
        
           alt="Flowbite React Logo"
           onClick={() => router.push('./pages/HomePage')}
+          onClick={() => router.push('./pages/HomePage')}
         />
+        <h1 className="txtOrange text-center lg:text-4xl md:text-3xl text-2xl juraBold mt-0 md:mt-2 pl-1 md:pl-5 " onClick={() => router.push('./pages/HomePage')}>
         <h1 className="txtOrange text-center lg:text-4xl md:text-3xl text-2xl juraBold mt-0 md:mt-2 pl-1 md:pl-5 " onClick={() => router.push('./pages/HomePage')}>
           {" "}
           Strike <span className="text-white ">Showdown</span>
