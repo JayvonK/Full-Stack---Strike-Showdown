1ebc4d24f0f17766b29c53708d5ad053c4481025
JayvonK <jaykhuth@gmail.com>
2024-05-03 07:12:11 -0700

    vercel fix 2

--- src/components/PageComponents/MessagingBubbleComponent.tsx
+++ src/components/PageComponents/MessagingBubbleComponent.tsx
@@ -11,7 +11,7 @@ const MessagingBubbleComponent = (props: { isSender: boolean, delivered: boolean
 
                         <div className=''>
                             <div className="flex justify-end">
-                                <h1 className='juraBold sm:text-2xl text-lg bg-[#FF7A00] sm:px-5 sm:py-2 px-3 py-1 rounded-lg max-w-full break-words'>What's up?</h1>
+                                <h1 className='juraBold sm:text-2xl text-lg bg-[#FF7A00] sm:px-5 sm:py-2 px-3 py-1 rounded-lg max-w-full break-words'>Whats up?</h1>
                             </div>
                             <div className='flex justify-end'>
                                 <p className='text-[#BFBFBF] jura sm:text-xl text-base sm:py-2 py-1'>Delivered</p>
