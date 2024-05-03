c03025eee5a3bb51a9c6f13d30d687ce6ec344b7
JayvonK <jaykhuth@gmail.com>
2024-05-03 07:12:11 -0700

    vercel fix 2

--- src/app/pages/ModalTestPage/page.tsx
+++ src/app/pages/ModalTestPage/page.tsx
@@ -11,20 +11,20 @@ import ViewProfileModalComponent from '@/components/PageComponents/ModalComponen
 
 
 function page() {
-    const [openModal, setOpenModal] = useState(true);
+    // const [openModal, setOpenModal] = useState(true);
   return (
 
     <div>
-       <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
+       {/* <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
        <Modal dismissible show={openModal} onClose={() => setOpenModal(false)} size={'4x1'}>
 
-       {/* <FriendsModalComponent/>  */}
-     {/* <InboxModalComponent/>   */}
+       <FriendsModalComponent/> 
+     <InboxModalComponent/>  
       <ProfileModalComponent userName={'Tommy201'} proNouns={'Salmon'} fullName={'alphaMale'} mainCenter={'Packed Man'} average={'1000'} highGames={'911'} highSeries={'111'} earnings={'4'} style={'quadruple handroll'} />  
- {/* <JoinSessionModalComponent />  */}
-{/* <ViewProfileModalComponent userName={'Briaaa'} proNouns={'hers/she'} record={'15'} mainCenter={'267'} average={'150'} highGames={'140'} highSeries={'100'} earnings={'2000'} style={'Righty'} email={'briaharrold@gmail.com'}/> */}
+ <JoinSessionModalComponent /> 
+<ViewProfileModalComponent userName={'Briaaa'} proNouns={'hers/she'} record={'15'} mainCenter={'267'} average={'150'} highGames={'140'} highSeries={'100'} earnings={'2000'} style={'Righty'} email={'briaharrold@gmail.com'}/>
 
-      </Modal>
+      </Modal> */}
     </div>
   )
 }
