0f9b7c5f9a51bb6a965bfcf446ce161090da2d0b
JayvonK <jaykhuth@gmail.com>
2024-05-10 15:52:56 -0700

    search modal ternary

--- src/components/PageComponents/HomePage/Modals/ConfirmationModal.tsx
+++ src/components/PageComponents/HomePage/Modals/ConfirmationModal.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const ConfirmationModal = (props: {closeConfirmation: () => void, deleteMatch: () => void}) => {
     return (
-        <div className='bg-white rounded-sm p-8'>
+        <div className='bg-white rounded-lg p-8'>
             <h1 className='text-3xl text-center mt-4 mb-2 juraBold'>Are you sure?</h1>
             <p className='text-xl mb-14 jura text-center'>Are you sure you want to delete your post? This action cannot be undone</p>
 
