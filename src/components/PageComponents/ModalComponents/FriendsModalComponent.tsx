63fd6526a0f40ff178800116ec3c9a4ad85761bd
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-05-16 16:30:27 -0700

    friend addition Correct commit!!!

--- src/components/PageComponents/ModalComponents/FriendsModalComponent.tsx
+++ src/components/PageComponents/ModalComponents/FriendsModalComponent.tsx
@@ -2,24 +2,80 @@ import React from 'react'
 import UserMinus from "../../../../public/images/UserMinus.png";
 import ProfilePic from "../../../../public/images/Ellipse 16.png";
 import { useState } from 'react';
-import { Modal, Button } from 'flowbite-react';
+import { Modal, Button} from 'flowbite-react';
 import "../../../app/css/LoginPageAndHome.css";
 import FriendBoxComponent from '../HomePage/FriendBoxComponent';
 
 const FriendsModalComponent = (props: {closeModal: () => void}) => {
     const [openModal, setOpenModal] = useState(false);
   return (
-  <>
-  <div className='px-10 pt-10'>
+  <div className='p-4  bg-white rounded-lg'>
+  <div>
   <h3
             className=" mb-6    text-md md:text-2xl  lg:text-3xl w-32   md:w-32  lg:w-36   rounded-xl  md:rounded-lg  bg-orange-500   hover:!bg-orange-500 text-center text-black jura px-4 pt-3 pb-3"
           >
             Friends
           </h3>
   </div>
-<div className=' grid grid-cols-3 max-h-[450px] justify-center gap-y-6 gap-x-4 overflow-auto'>
+  <div className=' grid lg:grid-cols-2 max-h-[450px] justify-center gap-6 overflow-y-auto'>
 
 <div>
+  <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
+        throw new Error('Function not implemented.');
+      } } message={function (): void {
+        throw new Error('Function not implemented.');
+      } }></FriendBoxComponent>
+      </div>
+      <div>
+  <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
+        throw new Error('Function not implemented.');
+      } } message={function (): void {
+        throw new Error('Function not implemented.');
+      } }></FriendBoxComponent>
+      </div>
+      <div>
+  <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
+        throw new Error('Function not implemented.');
+      } } message={function (): void {
+        throw new Error('Function not implemented.');
+      } }></FriendBoxComponent>
+      </div>
+      <div>
+  <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
+        throw new Error('Function not implemented.');
+      } } message={function (): void {
+        throw new Error('Function not implemented.');
+      } }></FriendBoxComponent>
+      </div>
+      <div>
+  <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
+        throw new Error('Function not implemented.');
+      } } message={function (): void {
+        throw new Error('Function not implemented.');
+      } }></FriendBoxComponent>
+      </div>
+      <div>
+  <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
+        throw new Error('Function not implemented.');
+      } } message={function (): void {
+        throw new Error('Function not implemented.');
+      } }></FriendBoxComponent>
+      </div>
+      <div>
+  <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
+        throw new Error('Function not implemented.');
+      } } message={function (): void {
+        throw new Error('Function not implemented.');
+      } }></FriendBoxComponent>
+      </div>
+      <div>
+  <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
+        throw new Error('Function not implemented.');
+      } } message={function (): void {
+        throw new Error('Function not implemented.');
+      } }></FriendBoxComponent>
+      </div>
+      <div>
   <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
         throw new Error('Function not implemented.');
       } } message={function (): void {
@@ -36,18 +92,19 @@ const FriendsModalComponent = (props: {closeModal: () => void}) => {
    
       </div>
       
-
-          <div className="flex justify-end px-10 py-10">
+  
+          <div className="flex justify-end pt-4">
             <Button
-              className="  mt-3 bg-orange-500  w-20 md:w-36 rounded-xl  md:rounded-2xl  hover:!bg-orange-500 text-black jura"
+              className="  mt-3 bg-orange-500  text-xl md:text-2xl  lg:text-3xl md:w-32  lg:w-36  rounded-xl  md:rounded-2xl  hover:!bg-orange-500 text-black jura"
               
               onClick={props.closeModal}
             >
               <h3 className=" text-lg md:text-2xl   lg:text-3xl md:w-32  lg:w-36  rounded-xl  md:rounded-2xl ">Close</h3>
             </Button>
           </div>
+
       
-      </>
+      </div>
   )
 }
 
