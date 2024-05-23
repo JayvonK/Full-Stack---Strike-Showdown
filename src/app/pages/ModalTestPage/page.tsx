32b95c8eb81708853c74048ffd99ee554937117f
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-05-23 05:37:40 -0700

    Friends box responsivness FINALLY DONEEEEEEEE

---
+++ src/app/pages/ModalTestPage/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+import FriendsModalComponent from '@/components/PageComponents/ModalComponents/FriendsModalComponent'
+import { Button, Modal } from 'flowbite-react'
+import React, { useState } from 'react'
+
+function page() {
+    const [openModal, setOpenModal] = useState(true);
+  return (
+    <div>
+ <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
+      <Modal show={openModal} onClose={() => setOpenModal(false)}size={"md"}>
+      <FriendsModalComponent closeModal={function (): void {
+              throw new Error('Function not implemented.')
+          } }/>
+          </Modal>
+          
+    </div>
+  )
+}
+
+export default page
