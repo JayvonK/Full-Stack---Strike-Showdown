fe036c2c72bc5292852aea189478242c391b3ab1
Briaharrold <94560548+Briaharrold@users.noreply.github.com>
2024-05-23 05:37:40 -0700

    Friends box responsivness FINALLY DONEEEEEEEE

--- src/components/PageComponents/FriendBoxComponent.tsx
+++ src/components/PageComponents/FriendBoxComponent.tsx
@@ -1,7 +1,6 @@
+import { Button } from "flowbite-react";
 import React from "react";
-import UserMinus from "../../../public/images/UserMinus.png";
-import ProfilePic from "../../../public/images/Ellipse 16.png";
-import Image from "next/image";
+
 function FriendBoxComponent(props: {
   userName: string;
   userImage: string;
@@ -9,50 +8,49 @@ function FriendBoxComponent(props: {
   message: () => void;
 }) {
   return (
-    <div className="  py-2 bg-black md:w-48  lg:w-54 xl:w-64 items-center mx-auto   rounded-2xl   ">
-      <div className="grid  grid-flow-col-dense gap-2  justify-center items-center    ">
+    <div className="  py-4 bg-black  w-80  md:w-64  lg:w-52 xl:w-64 items-center mx-auto   rounded-2xl   ">
+      <div className="grid  grid-flow-col gap-10  md:gap-2 lg:mx-2  justify-center items-center    ">
         <div className=" grid justify-center mr-1  ml-0 md:mr-1 md:ml-1 lg:mr-2 xl:mr-5 shrink-0">
           <img
-            className=" h-9 w-9 md:h-12 md:w-12   lg:!h-14 lg:!w-14 "
-            src={ProfilePic.src}
+            className=" min-h-14 min-w-14 lg:min-w-10 lg:min-h-10  "
+            src="/images/Profile.png"
           ></img>
         </div>
         <div className=" ">
           <div
-            className="flex flex-col-2 justify-end
+            className="flex flex-col-2 justify-start
        md:justify-center lg:justify-start  items-center"
           >
-            <div className="grid justify-end md:justify-center lg:justify-start  items-center">
+            <div className="grid justify-end md:justify-center lg:justify-start items-center">
               <img
                 data-nimg="intrinsic"
                 onClick={() => {}}
-                src={UserMinus.src}
-                className="pr-2 h-4 w-6  md:!h-5 md:!w-7 "
+                src="/images/UserMinus.png"
+                className="pr-2 h-5 w-7  "
               ></img>
             </div>
 
             <div className="">
-              <h1 className="text-white jura truncat text-sm md:text-md lg:text-lg xl:text-xl font-bold overflow-y-hidden ">
+              <h1 className="text-white jura  text-xl  md:text-md lg:text-lg xl:text-xl font-bold overflow-auto-hidden ">
                 BowlerDude
               </h1>
             </div>
           </div>
           <div className=" grid gap-0 md:gap-1 grid-wrap justify-center pr-4  md:justify-center lg:justify-start ">
             <div>
-              <a
-                href="#"
-                className=" items-center  text-xs truncate px-2.5  md:px-6 py-0 md:py-1 text-center xl:text-sm font-medium bg-orange-500   hover:!bg-orange-500 text-black jura"
+              <button
+                className=" items-center    min-w-40  lg:min-w-10 xl:min-w-0 text-xl  rounded-none  md:px-6 py-0  text-center lg:text-sm  xl:text-sm font-medium bg-orange-500   hover:!bg-orange-500 text-black jura"
               >
                 Challenge
-              </a>
+              </button>
             </div>
             <div>
-              <a
-                href="#"
-                className="items-center truncate text-xs  xl:text-sm  px-3.5   md:px-7  py-0 md:py-1 text-center  font-medium bg-orange-500  hover:!bg-orange-500 text-black jura"
+              <button
+            
+                className="items-center  rounded-none text-xl   min-w-40 lg:!min-w-5 xl:text-sm lg:text-sm   md:px-7  py-0  text-center  font-medium bg-orange-500   hover:!bg-orange-500 text-black jura"
               >
-                Message
-              </a>
+                Message 
+              </button>
             </div>
           </div>
         </div>
--- src/components/PageComponents/ModalComponents/FriendsModalComponent.tsx
+++ src/components/PageComponents/ModalComponents/FriendsModalComponent.tsx
@@ -13,12 +13,12 @@ const FriendsModalComponent = (props: {closeModal: () => void}) => {
   <div className='p-4  bg-white rounded-lg'>
   <div>
   <h3
-            className=" mb-6    text-lg md:text-xl   lg:text-2xl w-32   md:w-32  lg:w-36   rounded-lg  md:rounded-lg  bg-orange-500   hover:!bg-orange-500 text-center text-black jura px-4 py-1"
+            className=" mb-6    text-xl  w-40 lg:text-3xl  rounded-lg  md:rounded-lg  bg-orange-500 px-5 py-3   text-center text-black jura "
           >
             Friends
           </h3>
   </div>
-<div className=' grid grid-cols-2 md:grid-cols-3   justify-evenly  max-h-[450px]  gap-y-4 gap-x-2 overflow-auto'>
+<div className=' grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 xl:grid-col-3 xl:gap-5  2xl:grid-cols-3  justify-evenly  max-h-[450px]  gap-y-4 gap-x-2 overflow-auto'>
 
 <div>
   <FriendBoxComponent userName={''} userImage={''} challengeBtn={function (): void {
@@ -123,13 +123,13 @@ const FriendsModalComponent = (props: {closeModal: () => void}) => {
       
   
           <div className="flex justify-end pt-4">
-            <a
+            <Button
               className="    bg-orange-500 text-lg md:text-xl py-1  lg:text-2xl md:w-32  lg:w-36  rounded-lg  hover:!bg-orange-500 text-black jura"
               
               onClick={props.closeModal}
             >
-              <h3 className=" text-lg md:text-xl px-4   lg:text-2xl md:w-32  lg:w-36  text-center  rounded-lg ">Close</h3>
-            </a>
+              <h3 className=" text-xl   lg:text-3xl md:w-32  lg:w-36  text-center  rounded-lg ">Close</h3>
+            </Button>
           </div>
 
       
