eedec351bd1a32ff9486f8f345b235aa84f7451c
JayvonK <jaykhuth@gmail.com>
2024-05-25 22:15:15 -0700

    fixing vercel issue

--- src/app/pages/ModalTestPage/page.tsx
+++ src/app/pages/ModalTestPage/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import FriendsModalComponent from '@/components/PageComponents/ModalComponents/FriendsModalComponent'
 import { Button, Modal } from 'flowbite-react'
 import React, { useState } from 'react'
 
@@ -9,9 +8,9 @@ function page() {
     <div>
  <Button onClick={() => setOpenModal(true)}>Toggle modal</Button>
       <Modal show={openModal} onClose={() => setOpenModal(false)}size={"md"}>
-      <FriendsModalComponent closeModal={function (): void {
+      {/* <FriendsModalComponent closeModal={function (): void {
               throw new Error('Function not implemented.')
-          } }/>
+          } }/> */}
           </Modal>
           
     </div>
